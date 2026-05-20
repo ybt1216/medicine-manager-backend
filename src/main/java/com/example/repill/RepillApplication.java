d777c2e86af419bf118c8607e93f643031a0efe8
KimYongBin <adjtny@gmail.com>
2026-05-20 18:48:55 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/repill/RepillApplication.java
@@ -0,0 +1,13 @@
+package com.example.repill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repill/RepillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
